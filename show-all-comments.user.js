da13492d4e698483bb9e4cb7047f86bb80036610
myouisaur <myouisaur@gmail.com>
2025-09-16 05:58:03 +0800

    1.4
    Removed console log message

--- show-all-comments.user.js
+++ show-all-comments.user.js
@@ -2,7 +2,7 @@
 // @name         [Facebook] 'All comments' by default
 // @namespace    https://github.com/myouisaur/Facebook
 // @icon         https://static.xx.fbcdn.net/rsrc.php/y1/r/ay1hV6OlegS.ico
-// @version      1.3
+// @version      1.4
 // @description  Automatically selects "All comments" on Facebook comment.
 // @author       Xiv
 // @match        *://*.facebook.com/*
@@ -117,5 +117,4 @@
     // === Boot all modules ===
     scriptBoot();
     
-console.log('[Facebook Show-All-Comments] loaded!');
 })();
